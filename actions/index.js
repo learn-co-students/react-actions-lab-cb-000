5f0ae9226db533a4a0c6a53ca8e00462bcd7a297
joel <joelbitar@protonmail.com>
2017-11-09 23:41:36 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -3,38 +3,71 @@
 function addColumn (e) {
   e.preventDefault();
 
-	const x = this.state.table.map(function(row) {
-    return row.push('');
-  })
+	var updatedTable = this.state.table.map(x => x.concat(''));
 
-  debugger
   this.setState({
     table: updatedTable
   })
 }
 
 function addRow (e) {
-	
+  e.preventDefault();
+
+  this.setState({
+    table: [
+      ...this.state.table,
+      this.state.table[0].map(() => '')
+    ]
+  })
 }
 
 function changeCell (rowIndex, columnIndex, e) {
-  
+  const updatedRow = [...this.state.table[rowIndex]];
+  updatedRow.splice(columnIndex, 1, e.target.value);
+
+  const updatedTable = [...this.state.table.slice()];
+  updatedTable.splice(rowIndex, 1, updatedRow);
+
+  this.setState({
+    table: updatedTable
+  });
 }
 
 function focusCell (rowIndex, columnIndex) {
-  
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  });
 }
 
 function blurCell () {
-  
+  this.setState({
+    focused: null
+  });
 }
 
 function removeRow(e) {
- 
+  e.preventDefault();
+
+  if (this.state.table.length > 1) {
+    const updatedTable = this.state.table.slice(0,-1)
+
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 function removeColumn (e) {
-  
+  e.preventDefault();
+
+  if (this.state.table[0].length > 1) {
+    const updatedTable = this.state.table
+      .map(row => row.slice(0, -1));
+
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 module.exports = {
