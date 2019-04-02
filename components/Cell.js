d627142818dacec875ffc4cb41469be509d232f3
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 06:27:18 +0000

    Done.

--- components/Cell.js
+++ components/Cell.js
@@ -3,6 +3,14 @@
 import React from 'react';
 
 export default class Cell extends React.Component {
+  constructor(props){
+    super(props);
+  }
   render () {
+    return (
+      <td className='cell'>
+        <input value={this.props.cell} onFocus={this.props.onFocus} onChange={this.props.onChange} />
+      </td>
+    )
   }
 }
--- components/Row.js
+++ components/Row.js
@@ -5,5 +5,16 @@ import Cell from './Cell';
 
 export default class Row extends React.Component {
   render () {
+    return(
+      <tr className="row">
+        {
+          this.props.row.map(function(cell, index){
+            return (
+              <Cell key={index} cell={cell} onChange={this.props.onChange.bind(null, columnIndex)} onFocus={this.props.onFocus.bind(null, columnIndex)} />
+            )
+          })
+        }
+      </tr>
+    )
   }
 }
