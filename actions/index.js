5ec94d261c22a3f098b9d0d372725648948ae18e
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-10 09:07:55 +0000

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,71 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault()
+  var newTable = this.state.table
+  for(var i = 0; i < newTable.length; i++){
+    newTable[i].push("")
+  }
+  this.setState({
+    table: newTable
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault()
+  var newTable = this.state.table
+  var row = []
+  for(var i = 0; i < newTable[0].length; i ++){
+    row.push("")
+  }
+  newTable.push(row)
+  this.setState({
+    table: newTable
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  var newTable = this.state.table
+  newTable[rowIndex][columnIndex] = ev.target.value
+  this.setState({
+    table: newTable
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault()
+  if (this.state.table.length > 1){
+    var newTable = this.state.table
+    newTable.pop()
+    this.setState({
+      table: newTable
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault()
+  var newTable = this.state.table
+  if (this.state.table[0].length > 1){
+    for(var i = 0; i < newTable.length; i++){
+      newTable[i].pop()
+    }
+    this.setState({
+      table: newTable
+    })
+  }
 }
 
 export default {
