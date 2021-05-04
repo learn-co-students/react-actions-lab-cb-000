067ee1b1126bd39e1df2037e768ef8c7f99110f4
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-04 15:58:45 +0000

    Done.

--- components/Row.js
+++ components/Row.js
@@ -5,5 +5,19 @@ import Cell from './Cell';
 
 export default class Row extends React.Component {
   render () {
+    return (
+      <tr className='row'>
+        {
+          this.props.row.map((value, i) =>
+            <Cell
+              key={i}
+              cell={value}
+              onChange={this.props.onChange.bind(null, i)}
+              onFocus={this.props.onFocus.bind(null, i)}
+            />
+          )
+        }
+      </tr>
+    )
   }
 }
